a1bf1d1da6f076a4dbb67388314115baa19f7141
Pizzetta <paul.pizzetta@gmail.ch>
2025-03-13 14:48:33 +0100

    created test_blob.py

---
+++ tests/test_blob.py
@@ -0,0 +1,20 @@
+import arcade
+import constants
+from blob import *
+from gameview import GameView
+
+
+#le test marche principalement car il n'y a aucun blob sur la map 
+
+def test_key(window: arcade.Window) -> None:
+    view = GameView()
+    window.show_view(view)
+
+    for x in range (0, 128)  :
+        for blob in view.blob_list :
+            assert view.check_for_collision_with_list(blob, view.wall_list) == []
+            blob.center_y -= 10
+            assert view.check_for_collision_with_list(blob, view.wall_list) != []
+            blob.center_y += 10
+            window.test(1)
+
