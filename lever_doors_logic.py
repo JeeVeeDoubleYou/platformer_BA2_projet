d5ab36e50de633d02f40217d070c0c1455847231
Gaëlle Viso <gaelleviso@gmail.com>
2025-05-24 19:16:17 +0200

    Refactored map with new lever_doors_logic class

---
+++ lever_doors_logic.py
@@ -0,0 +1,122 @@
+from door import Door
+from lever import Lever
+
+
+class LeverDoorsLogic :
+
+    def __init__(self) -> None :
+        pass
+
+    def __action_linking(self, switch_on_or_off: list[dict[str,object]],
+                        map_doors: list[list[Door|None]]) ->tuple[list[Door],list[Door],bool]:
+        one_time_use : bool = False
+        list_close: list[Door] = []
+        list_open: list[Door] = []
+        for element in switch_on_or_off:
+            if not isinstance(element,dict):
+                raise Exception("A switch_on action is incorect")
+            assert(isinstance(element,dict))
+            match element:
+                case {'action':'disable'}:
+                    one_time_use = True
+                case {'x': int() as x, 'y': int() as y,'action':'open-gate'}:
+                    door_in_map = map_doors[y][x]
+                    if not isinstance(door_in_map, Door):
+                        raise Exception(f"There is no door at (x, y) = {(x, y)}")
+                    list_open.append(door_in_map)
+                case {'x': int() as x, 'y': int() as y,'action':'close-gate'}:
+                    door_in_map = map_doors[y][x]
+                    if not isinstance(door_in_map, Door):
+                        raise Exception(f"There is no door at (x, y) = {(x, y)}")
+                    list_close.append(door_in_map)
+        return (list_open, list_close, one_time_use)
+            
+    # ATTENTION : Is *never* used
+    def __add_door_to_list(self, map_doors : list[list[Door|None]], x : int, y : int, list : list[Door]) -> None :
+        """Takes the [y][x] element in map_doors, checks that it's a door and adds it to the list."""
+        door = map_doors[y][x]
+        if not isinstance(door, Door):
+            raise Exception(f"There is no door at (x, y) = {(x, y)}")
+        assert(isinstance(door, Door))
+        list.append(door)
+
+    # ATTENTION : Wanted to refactor the switch underneath but i have problems with dict type anotation in function 
+    # def set_lever_action(self, dict_key : str, open_list : list[Door], close_list : list[Door], map_doors : list[list[Door|None]]):
+
+    def lever_door_linking(self, ymal_part : dict[str,object], map_doors : list[list[Door|None]], map_levers : list[list[Lever|None]]) -> None:
+        """Make the levere able to open their door"""
+        try:
+            match ymal_part:
+                case {'switches': list() as switches}:
+                    for switch in switches:
+                        if not isinstance(switch,dict):
+                            raise Exception("the switch list is incorect")
+                        assert(isinstance(switch,dict))
+                        activation_close : list[Door] = [] 
+                        activation_open : list[Door] = []  
+                        deactivation_close : list[Door] = [] 
+                        deactivation_open : list[Door] = [] 
+                        start_on : bool = False
+                        on_deactivate : bool = False
+                        off_deactivate : bool = False
+                        match switch:
+                            case {'switch_on': list() as switch_on}:
+                                tuple = self.__action_linking(switch_on, map_doors)
+                                activation_open = tuple[0] 
+                                activation_close = tuple[1] 
+                                on_deactivate = tuple[2]
+                                #for element in switch_on:
+                                #    if not isinstance(element,dict):
+                                #        raise Exception("A switch_on action is incorect")
+                                #    assert(isinstance(switch,dict))
+                                #    match element:
+                                #        case {'action':'disable'}:
+                                #            one_time_use = True
+                                #        case {'x': int() as x, 'y': int() as y,'action':'open-gate'}:
+                                #            self.add_door_to_list(map_doors, x, y, activation_open)
+                                #        case {'x': int() as x, 'y': int() as y,'action':'close-gate'}:
+                                #            self.add_door_to_list(map_doors, x, y, activation_close)
+                        match switch:
+                            case {'switch_off': list() as switch_off}:
+                                        tuple = self.__action_linking(switch_off, map_doors)
+                                        deactivation_open = tuple[0] 
+                                        deactivation_close = tuple[1] 
+                                        off_deactivate = tuple[2]
+                        if switch.get('state') == True:
+                            start_on = True
+                                #for element in switch_off:
+                                #    if not isinstance(element,dict):
+                                #        raise Exception("A switch_off action is incorect")
+                                #    assert(isinstance(switch, dict))
+                                #    match element:
+                                #        case {'action':'disable'}:
+                                #            one_time_use = True
+                                #        case {'x': int() as x, 'y': int() as y,'action':'open-gate'}:
+                                #            self.add_door_to_list(map_doors, x, y, deactivation_open)    
+                                #        case {'x': int() as x, 'y': int() as y,'action':'close-gate'}:
+                                #            self.add_door_to_list(map_doors, x, y, deactivation_close)        
+                        match switch:
+                            case {'x': int() as x, 'y': int() as y}:
+                                lever_in_map = map_levers[y][x]
+                                if isinstance(lever_in_map, Lever):
+                                    lever : Lever = lever_in_map
+                                    lever.link_doors(activation_close ,activation_open, deactivation_close,
+                                                      deactivation_open, on_deactivate, off_deactivate, start_on)
+                                else: Exception(f"There is no lever at (x, y) = {(x, y)}")
+                            case _ :
+                                raise Exception("Please precise where the lever is suposed to be with integer coordinate")
+            match ymal_part:
+                case {'gates': list() as doors}:
+                    for door in doors:
+                        if not isinstance(door, dict):
+                            raise Exception("A door action is incorect")
+                        assert(isinstance(door,dict))
+                        match door:
+                            case {'x': int() as x, 'y': int() as y,'state':'open'}:
+                                door_in_map = map_doors[y][x]
+                                if isinstance(door_in_map,Door):
+                                    door_in_map.open()
+                                else: raise Exception(f"There is no door at (x, y) = {(x, y)}")
+                                
+        except ValueError :
+            pass
--- map.py
+++ map.py
@@ -16,6 +16,7 @@ from ghost import Ghost
 from frog import Frog
 from map_movement import MapMovement
 from helper import matrix_line_num_to_arcade
+from lever_doors_logic import LeverDoorsLogic
 
 
 
@@ -102,121 +103,6 @@ class Map :
             raise Exception(f"Width and height should be defined and non-zero in configuration of file {self.__current_map_name}")
         if (self.__width < 0 or self.__height < 0) :
             raise Exception("Width and height should be positive numbers")
-
-    def action_linking(self, switch_on_or_off: list[dict[str,object]],
-                        map_doors: list[list[Door|None]]) ->tuple[list[Door],list[Door],bool]:
-        one_time_use : bool = False
-        list_close: list[Door] = []
-        list_open: list[Door] = []
-        for element in switch_on_or_off:
-            if not isinstance(element,dict):
-                raise Exception("A switch_on action is incorect")
-            assert(isinstance(element,dict))
-            match element:
-                case {'action':'disable'}:
-                    one_time_use = True
-                case {'x': int() as x, 'y': int() as y,'action':'open-gate'}:
-                    door_in_map = map_doors[y][x]
-                    if not isinstance(door_in_map,Door):
-                        raise Exception("There is no door at x= ",x," y= ",y)
-                    list_open.append(door_in_map)
-                case {'x': int() as x, 'y': int() as y,'action':'close-gate'}:
-                    door_in_map = map_doors[y][x]
-                    if not isinstance(door_in_map,Door):
-                        raise Exception("There is no door at x= ",x," y= ",y)
-                    list_close.append(door_in_map)
-        return (list_open, list_close, one_time_use)
-            
-     
-
-    def add_door_to_list(self, map_doors : list[list[Door|None]], x : int, y : int, list : list[Door]) -> None :
-        """Takes the [y][x] element in map_doors, checks that it's a door and adds it to the list."""
-        door = map_doors[y][x]
-        if not isinstance(door, Door):
-            raise Exception(f"There is no door at (x, y) = {(x, y)}")
-        assert(isinstance(door, Door))
-        list.append(door)
-
-    # ATTENTION : Wanted to refactor the switch underneath but i have problems with dict type anotation in function 
-    # def set_lever_action(self, dict_key : str, open_list : list[Door], close_list : list[Door], map_doors : list[list[Door|None]]):
-
-    def lever_door_linking(self, map_doors : list[list[Door|None]], map_levers : list[list[Lever|None]]) -> None:
-        """Make the levere able to open their door"""
-        try:
-            match self.__ymal_part:
-                case {'switches': list() as switches}:
-                    for switch in switches:
-                        if not isinstance(switch,dict):
-                            raise Exception("the switch list is incorect")
-                        assert(isinstance(switch,dict))
-                        activation_close : list[Door] = [] 
-                        activation_open : list[Door] = []  
-                        deactivation_close : list[Door] = [] 
-                        deactivation_open : list[Door] = [] 
-                        start_on : bool = False
-                        on_deactivate : bool = False
-                        off_deactivate : bool = False
-                        match switch:
-                            case {'switch_on': list() as switch_on}:
-                                tuple = self.action_linking(switch_on, map_doors)
-                                activation_open = tuple[0] 
-                                activation_close = tuple[1] 
-                                on_deactivate = tuple[2]
-                                #for element in switch_on:
-                                #    if not isinstance(element,dict):
-                                #        raise Exception("A switch_on action is incorect")
-                                #    assert(isinstance(switch,dict))
-                                #    match element:
-                                #        case {'action':'disable'}:
-                                #            one_time_use = True
-                                #        case {'x': int() as x, 'y': int() as y,'action':'open-gate'}:
-                                #            self.add_door_to_list(map_doors, x, y, activation_open)
-                                #        case {'x': int() as x, 'y': int() as y,'action':'close-gate'}:
-                                #            self.add_door_to_list(map_doors, x, y, activation_close)
-                        match switch:
-                            case {'switch_off': list() as switch_off}:
-                                        tuple = self.action_linking(switch_off, map_doors)
-                                        deactivation_open = tuple[0] 
-                                        deactivation_close = tuple[1] 
-                                        off_deactivate = tuple[2]
-                        if switch.get('state') == True:
-                            start_on = True
-                                #for element in switch_off:
-                                #    if not isinstance(element,dict):
-                                #        raise Exception("A switch_off action is incorect")
-                                #    assert(isinstance(switch, dict))
-                                #    match element:
-                                #        case {'action':'disable'}:
-                                #            one_time_use = True
-                                #        case {'x': int() as x, 'y': int() as y,'action':'open-gate'}:
-                                #            self.add_door_to_list(map_doors, x, y, deactivation_open)    
-                                #        case {'x': int() as x, 'y': int() as y,'action':'close-gate'}:
-                                #            self.add_door_to_list(map_doors, x, y, deactivation_close)        
-                        match switch:
-                            case {'x': int() as x, 'y': int() as y}:
-                                lever_in_map = map_levers[y][x]
-                                if isinstance(lever_in_map, Lever):
-                                    lever : Lever = lever_in_map
-                                    lever.link_doors(activation_close ,activation_open, deactivation_close,
-                                                      deactivation_open, on_deactivate, off_deactivate, start_on)
-                                else: Exception(f"There is no lever at (x, y) = {(x, y)}")
-                            case _ :
-                                raise Exception("Please precise where the lever is suposed to be with integer coordinate")
-            match self.__ymal_part:
-                case {'gates': list() as doors}:
-                    for door in doors:
-                        if not isinstance(door, dict):
-                            raise Exception("A door action is incorect")
-                        assert(isinstance(door,dict))
-                        match door:
-                            case {'x': int() as x, 'y': int() as y,'state':'open'}:
-                                door_in_map = map_doors[y][x]
-                                if isinstance(door_in_map,Door):
-                                    door_in_map.open()
-                                else: raise Exception(f"There is no door at (x, y) = {(x, y)}")
-                                
-        except ValueError :
-            pass
 
     def __file_to_matrix(self) -> None :
         """Turns map file into a matrix"""
@@ -322,7 +208,7 @@ class Map :
             raise Exception("Player must have a starting point")
         if self.__next_map is not None and not end_is_placed :
             raise Exception("The file sets the next map but no end to the level")
-        self.lever_door_linking(map_doors,map_levers) 
+        LeverDoorsLogic().lever_door_linking(self.__ymal_part, map_doors, map_levers) 
         
     # ATTENTION : Should be a property?
     def get_player_coordinates(self) -> tuple[int, int] :
@@ -332,9 +218,11 @@ class Map :
     def get_next_map(self) -> str | None :
         return self.__next_map
     
-    def valid_dict(self,dict: dict[str,object], key: str, type_in_dict: type ) -> bool:
-        """Return true if the dict have the key and the correct value type associated with that key """
-        if key in dict:
-            if isinstance(dict[key],type_in_dict):
-                return True
-        return False
+    # ATTENTION : Isn't used *anywhere*
+
+    # def valid_dict(self,dict: dict[str,object], key: str, type_in_dict: type ) -> bool:
+    #     """Return true if the dict have the key and the correct value type associated with that key """
+    #     if key in dict:
+    #         if isinstance(dict[key],type_in_dict):
+    #             return True
+    #     return False
