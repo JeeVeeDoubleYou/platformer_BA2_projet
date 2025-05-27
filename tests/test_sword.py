4109f7c0f339e7e3134dbea3d71bcf039ee018e4
Gaëlle Viso <gaelleviso@gmail.com>
2025-05-27 09:52:57 +0200

    Renamed conflicting test names, commented

--- tests/test_sword.py
+++ tests/test_sword.py
@@ -5,6 +5,7 @@ from gameview import GameView
 from player import Player
 from sword import Sword
 
+# Same tests, with swords pointing in different directions. Answers week 4, question 2.
 
 def test_sword(window: arcade.Window) -> None:
     view = GameView("maps/testing_maps/sword_test_map.txt")
@@ -12,10 +13,10 @@ def test_sword(window: arcade.Window) -> None:
     #verifie que l'épée tue le monstre
     view.on_mouse_press(1000, 300, arcade.MOUSE_BUTTON_LEFT, 0)
     assert(len(view.get_weapon_list) == 1)
-    window.test(150)
+    window.test(35)
     assert(isinstance(view.get_weapon_list[-1], Sword))  #verifie que l'arme est une épée
     view.on_mouse_release(128, 64, arcade.MOUSE_BUTTON_LEFT, 0)
-    window.test(50)    
+    window.test(1)    
     assert(len(view.get_weapon_list) == 0)
     assert(len(view.get_monster_list) == 0)
 
@@ -25,7 +26,7 @@ def test_wrong(window: arcade.Window) -> None:
     #verifie que l'épée ne tue pas le monstre
     view.on_mouse_press(0, 0, arcade.MOUSE_BUTTON_LEFT, 0)
     assert(len(view.get_weapon_list) == 1)
-    window.test(15)
+    window.test(35)
     assert(isinstance(view.get_weapon_list[-1], Sword))  #verifie que l'arme est une épée
     view.on_mouse_release(0, 0, arcade.MOUSE_BUTTON_LEFT, 0)
     window.test(1)    
