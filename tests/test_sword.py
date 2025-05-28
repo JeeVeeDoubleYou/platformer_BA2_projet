17be4950e230ced01474845c3ed2a4b52c6c3b9f
Pizzetta <paul.pizzetta@gmail.ch>
2025-05-28 14:41:58 +0200

    corrected bug in test_sword

--- tests/test_sword.py
+++ tests/test_sword.py
@@ -4,20 +4,20 @@ from blob import *
 from gameview import GameView
 from player import Player
 from sword import Sword
+from weapon import Weapon
 
 
 def test_sword(window: arcade.Window) -> None:
     view = GameView("maps/testing_maps/sword_test_map.txt")
     window.show_view(view)
     #verifie que l'épée tue le monstre
-    view.on_mouse_press(1000, 300, arcade.MOUSE_BUTTON_LEFT, 0)
-    assert(len(view.get_weapon_list) == 1)
-    window.test(150)
-    assert(isinstance(view.get_weapon_list[-1], Sword))  #verifie que l'arme est une épée
-    view.on_mouse_release(128, 64, arcade.MOUSE_BUTTON_LEFT, 0)
-    window.test(50)    
+    for i in range(10):
+        view.on_mouse_press(1000, 300, arcade.MOUSE_BUTTON_LEFT, 0)
+        window.test(10)
+        view.on_mouse_release(1000, 30000, arcade.MOUSE_BUTTON_LEFT, 0) 
     assert(len(view.get_weapon_list) == 0)
     assert(len(view.get_monster_list) == 0)
+
 
 def test_wrong(window: arcade.Window) -> None:
     view = GameView("maps/testing_maps/sword_test_map.txt")
